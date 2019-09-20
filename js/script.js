58318bf8cfca22c3d175ea525be32922051b1dc2
Maciej Kryczka <maciejkryczka@hotmail.com>
2019-09-20 18:40:03 +0200

    add side menu switch

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
 console.log('podpiete');
 
-function menuSwitch(visable) {
+function menuSwitch(hamb) {
   const menus = document.querySelectorAll('.menu');
   const sideBar = document.querySelector('.side-menu');
+  const shortMenu = document.querySelector('.sr-menu');
+  const fullMenu = document.querySelector('.fl-menu');
+  const medMenu = document.querySelector('.md-menu');
 
-  for(let men of menus){
-    men.classList.toggle('active', visable);
-    console.log('dziala');
+  //for(let men of menus){
+  //  men.classList.toggle('active', visable);
+  //}
+
+  if (hamb.classList.contains('ham-sr')){
+    shortMenu.classList.toggle('not-active');
+    sideBar.classList.add('size-fl');
+    sideBar.classList.remove('size-sr');
+    medMenu.classList.toggle('not-active');
+    medMenu.classList.toggle('active');
+  } else if (hamb.classList.contains('ham-md')){
+    shortMenu.classList.toggle('not-active');
+    sideBar.classList.remove('size-fl');
+    sideBar.classList.add('size-sr');
+    medMenu.classList.toggle('not-active');
+    medMenu.classList.toggle('active');
   }
-  console.log('side-bar to:', sideBar);
-    sideBar.classList.toggle('size');
 }
 
 const hamburgers = document.querySelectorAll('.hamburger');
@@ -17,7 +31,9 @@ const hamburgers = document.querySelectorAll('.hamburger');
 for(let hamb of hamburgers){
   hamb.addEventListener('click', function(e){
     e.preventDefault();
-    console.log('klik');
-    menuSwitch();
+    hamb = this;
+    console.log('hamb to', hamb);
+    menuSwitch(hamb);
+
   });
 }
