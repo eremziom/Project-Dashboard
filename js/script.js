26b33690fea0315fbec88a71b84fd4cc943a5643
Maciej Kryczka <maciejkryczka@hotmail.com>
2019-09-20 14:53:32 +0200

    fix top-bar media from right to left side

--- js/script.js
+++ js/script.js
@@ -1,19 +1,23 @@
 console.log('podpiete');
 
 function menuSwitch(visable) {
-  const menus = document.querySelectorAll('.menu')
+  const menus = document.querySelectorAll('.menu');
+  const sideBar = document.querySelector('.side-menu');
+
   for(let men of menus){
     men.classList.toggle('active', visable);
     console.log('dziala');
   }
+  console.log('side-bar to:', sideBar);
+    sideBar.classList.toggle('size');
 }
 
-const hamburgers = document.querySelectorAll('.hamburger')
+const hamburgers = document.querySelectorAll('.hamburger');
 
 for(let hamb of hamburgers){
   hamb.addEventListener('click', function(e){
     e.preventDefault();
     console.log('klik');
     menuSwitch();
-  })
+  });
 }
