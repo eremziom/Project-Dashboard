557c40c1afd9c4264ab844abe2ac764930749e27
Maciej Kryczka <maciejkryczka@hotmail.com>
2019-09-20 21:53:48 +0200

    fix side menu to slide onto container

--- js/script.js
+++ js/script.js
@@ -1,58 +1,29 @@
-console.log('podpiete');
-
-function menuSwitch(hamb) {
-  const menus = document.querySelectorAll('.menu');
-  const sideBar = document.querySelector('.side-menu');
-  const shortMenu = document.querySelector('.sr-menu');
-  const fullMenu = document.querySelector('.fl-menu');
-  const medMenu = document.querySelector('.md-menu');
-
-  //for(let men of menus){
-  //  men.classList.toggle('active', visable);
-  //}
-
-  if (hamb.classList.contains('ham-sr')){
-    shortMenu.classList.toggle('not-active');
-    sideBar.classList.add('size-fl');
-    sideBar.classList.remove('size-sr');
-    medMenu.classList.toggle('not-active');
-    medMenu.classList.toggle('active');
-  } else if (hamb.classList.contains('ham-md')){
-    shortMenu.classList.toggle('not-active');
-    sideBar.classList.remove('size-fl');
-    sideBar.classList.add('size-sr');
-    medMenu.classList.toggle('not-active');
-    medMenu.classList.toggle('active');
-  }
-}
-
 const hamburgers = document.querySelectorAll('.hamburger');
 
 for(let hamb of hamburgers){
   hamb.addEventListener('click', function(e){
     e.preventDefault();
     hamb = this;
-    console.log('hamb to', hamb);
-    //menuSwitch(hamb);
-    menuSwitch2(hamb);
+    menuSwitch(hamb);
   });
 }
 
-function menuSwitch2(hamb) {
+function menuSwitch(hamb) {
 
-  const sideBar = document.querySelector('.side-menu');
   const shortMenu = document.querySelector('.sr-menu');
   const fullMenu = document.querySelector('.fl-menu');
+  const contMenu = document.querySelector('.menu-container');
+  const hambFl = document.querySelector('.ham-fl');
 
   if (hamb.classList.contains('ham-sr')){
     shortMenu.classList.toggle('hide');
-    sideBar.classList.add('size-fl');
-    sideBar.classList.remove('size-sr');;
+    contMenu.classList.toggle('cont-size-fl');
     fullMenu.classList.toggle('show');
-  } else if (hamb.classList.contains('ham-fl')){
+    hambFl.classList.toggle('ham-md');
+  } else if (hamb.classList.contains('ham-md')){
     shortMenu.classList.toggle('hide');
-    sideBar.classList.remove('size-fl');
-    sideBar.classList.add('size-sr');
+    contMenu.classList.toggle('cont-size-fl');
     fullMenu.classList.toggle('show');
+    hambFl.classList.toggle('ham-md');
   }
 }
